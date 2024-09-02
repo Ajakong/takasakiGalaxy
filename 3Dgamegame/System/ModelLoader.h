53c7fed5159378e7b6ede059f1b051418f74e25b
Ajakong <135081816+Ajakong@users.noreply.github.com>
2024-09-02 16:56:22 +0900

    up

--- 3Dgamegame/System/ModelLoader.h
+++ 3Dgamegame/System/ModelLoader.h
@@ -1,29 +1,29 @@
-#pragma once
-#include<map>
-#include<string>
-class Model;
-
-class ModelLoader
-{
-private:
-	struct ModelInfo {
-		int handle;//ハンドル
-		bool used;//一度以上使用済みか？
-	};
-	std::map<std::string, ModelInfo> _pathAndModelInfoes;
-	int vsH_;
-	int psH_;
-public:
-	ModelLoader();
-	~ModelLoader();
-	///モデルデータのプリロード
-	///@param filepath ファイルパス
-	void PreloadModelData(const char* filepath);
-
-	///モデルデータをロードしてモデルオブジェクトを作る
-	///@param filepath ファイルパス
-	///@return モデルクラスポインタ
-	Model* CreateModel(const char* filepath);
-
-};
-
+//#pragma once
+//#include<map>
+//#include<string>
+//class Model;
+//
+//class ModelLoader
+//{
+//private:
+//	struct ModelInfo {
+//		int handle;//ハンドル
+//		bool used;//一度以上使用済みか？
+//	};
+//	std::map<std::string, ModelInfo> _pathAndModelInfoes;
+//	int vsH_;
+//	int psH_;
+//public:
+//	ModelLoader();
+//	~ModelLoader();
+//	///モデルデータのプリロード
+//	///@param filepath ファイルパス
+//	void PreloadModelData(const char* filepath);
+//
+//	///モデルデータをロードしてモデルオブジェクトを作る
+//	///@param filepath ファイルパス
+//	///@return モデルクラスポインタ
+//	Model* CreateModel(const char* filepath);
+//
+//};
+//
